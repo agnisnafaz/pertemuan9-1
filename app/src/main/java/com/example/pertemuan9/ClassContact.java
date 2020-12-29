b6cfe3a4408d45ca7342f8485535bf8d47b958bb
agnisnafaz <agnisnurafaz@gmail.com>
2020-12-29 15:33:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pertemuan9/ClassContact.java
@@ -0,0 +1,62 @@
+package com.example.pertemuan9;
+
+public class ClassContact {
+    private String nama, telepon, sosmed, alamat, foto;
+
+    public ClassContact(String nama, String telepon, String sosmed, String alamat, String foto){
+        this.nama = nama;
+        this.telepon = telepon;
+        this.sosmed = sosmed;
+        this.alamat = alamat;
+        this.foto = foto;
+    }
+
+    public String getNama(){
+
+        return nama;
+    }
+
+    public void setNama(String nama) {
+
+        this.nama = nama;
+    }
+
+    public String getTelepon(){
+
+        return telepon;
+    }
+
+    public void setTelepon(String telepon){
+        this.telepon = telepon;
+    }
+
+    public String getSosmed(){
+
+        return sosmed;
+    }
+
+    public void setSosmed(String sosmed){
+
+        this.sosmed = sosmed;
+    }
+
+    public String getAlamat(){
+
+        return alamat;
+    }
+
+    public void setAlamat(){
+
+        this.alamat = alamat;
+    }
+
+    public String getFoto(){
+
+        return foto;
+    }
+
+    public void setFoto(String foto){
+
+        this.foto = foto;
+    }
+}
